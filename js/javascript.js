cc02793a8bfdd0ff8976f8526388828215a0e0f6
Agung <indoside@live.nl>
2019-06-08 16:49:38 +0200

    Necessary files loaded

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+// var fullpageWrapper = document.querySelector('#fullpage');
+
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
